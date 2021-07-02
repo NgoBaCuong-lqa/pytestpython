950781eb85f29d92a413ca7938a8784d5d572991
unknown <Administrator@TEST-THUONGBT.lqa.local>
2021-07-02 18:46:30 +0700

    master

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,15 +1,18 @@
 import pytest
 from selenium import webdriver
-from Config.config import TestData
+
 
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(params=["chrome"], scope="class")
 def init_driver(request):
-    if request.param == "chorme":
+    global webdriver
+    if request.param == "chrome":
         web_driver = webdriver.Chrome(ChromeDriverManager().install())
-    request.cls.driver = web_driver
-    web_driver.implicitly_wait(10)
-    yield
-    web_driver.close()
+        # print(web_driver)
+        request.cls.driver = web_driver
+        yield web_driver
+        web_driver.close()
+
+
 
--- Tests/test_LoginPage.py
+++ Tests/test_LoginPage.py
@@ -3,15 +3,20 @@ import pytest
 from Config.config import TestData
 from Pages.LoginPage import LoginPage
 from Tests.test_base import BaseTest
+from Pages.BasePage import BasePage
 
+@pytest.mark.usefixtures('init_driver')
 
-class Text_Login(BaseTest):
-
-    def test_login_page_title(self):
-        self.loginPage = LoginPage(self.driver)
-        title =self.loginPage.get_title(TestData.LOGIN_PAGE_TITLE)
-        assert title == TestData.LOGIN_PAGE_TITLE
+class Test_Login(BasePage):
+    LOGIN_BUTTON = (By.ID, "u_0_d_dX")
+    # def test_login_page_title(self):
+    #     self.driver.get('https://www.google.com/')
+    #     loginPage = LoginPage(self.driver)
+    #     title = loginPage.get_title(TestData.LOGIN_PAGE_TITLE)
+    #     assert title == TestData.LOGIN_PAGE_TITLE
 
     def test_login(self):
+        self.driver.get('https://vi-vn.facebook.com/')
         self.loginPage = LoginPage(self.driver)
         self.loginPage.do_login(TestData.USER_NAME, TestData.PASSWORD)
+        self.do_click(self.LOGIN_BUTTON)
